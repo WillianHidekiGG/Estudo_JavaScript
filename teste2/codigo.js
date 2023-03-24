3c3cc10cd14a5546856042bdbae5cb5821af6f61
WolfWillian <willianhidekigg@gmail.com>
2023-03-24 20:53:31 -0300

    ArquivosIniciais

---
+++ teste2/codigo.js
@@ -0,0 +1,10 @@
+var a= 10;
+var b= 20;
+var c= a*b;
+alert("o valor é 1: "+ 10 );
+// alert(c);
+//no alerta já soma os valores//
+
+var nome;
+nome= prompt("insira seu nome");
+alert("Bem-Vindo " + nome);
