3ae8607d94c00619b3cebe17ae21a1eaeb85ee8a
WolfWillian <willianhidekigg@gmail.com>
2023-03-26 15:06:24 -0300

    Commit Inicial

---
+++ projects/tela_confirmar/confirma.js
@@ -0,0 +1,4 @@
+var sair = confirm("DEseja sair dessa pagina e ir para o Goggle?"); 
+if(sair == 1){
+    window.location = "https://www.google.com/webhp?authuser=3";
+}
---
+++ projects/tela_media/media.js
@@ -0,0 +1,9 @@
+var notaA = 8;
+var notaB = 7;
+
+var media = (notaA+notaB)/2;
+if(media => 7){
+  alert("APROVADO " + "media: " + media);
+}else{
+    alert("REPROVADO " + "media: " + media);
+}
