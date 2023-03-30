492b48ae3c6d0ab48d76ea7b2dee2f0435337aee
WolfWillian <willianhidekigg@gmail.com>
2023-03-29 21:34:50 -0300

    Alerta Modificado

--- projects/tela_alert/codigo.js
+++ projects/tela_alert/codigo.js
@@ -1,14 +1,14 @@
 function alert() {
-    Swal.fire({
-      showClass: {
-        popup: "animate__animated animate__fadeInDown",
-      },
-      hideClass: {
-        popup: "animate__animated animate__fadeOutUp",
-      },
-      icon: "success",
-      title: "FUNCIONOU!",
-      footer:
-        '<a href="https://github.com/WolfWillian/Estudo_JavaScript.git">Confira meu GitHub</a>',
-    });
+  Swal.fire({
+    showClass: {
+      popup: "animate__animated animate__fadeInDown",
+    },
+    hideClass: {
+      popup: "animate__animated animate__fadeOutUp",
+    },
+    icon: "success",
+    title: "FUNCIONOU!",
+    footer:
+      '<a href="https://github.com/WolfWillian/Estudo_JavaScript.git">Confira meu GitHub</a>',
+  });
 }
--- projects/tela_bem-Vindo/codigo.js
+++ projects/tela_bem-Vindo/codigo.js
@@ -1,10 +1,32 @@
-var a= 10;
-var b= 20;
-var c= a*b;
-alert("o valor é 1: "+ 10 );
-// alert(c);
-//no alerta já soma os valores//
+Nightly.isDark = true;
+
+settingsMenuHtml =
+  '<input type="checkbox" onclick="Nightly.isDark = !Nightly.isDark; setCSSLink()">';
+
+/*var a = 10;
+var b = 20;
+var c = a * b;
+alert("o valor é 1: " + 10);
+ alert(c);
+no alerta já soma os valores*/
 
 var nome;
-nome= prompt("insira seu nome");
-alert("Bem-Vindo " + nome);
+function alert() {
+Swal.fire({
+  title: 'Qual seu nome?',
+  input: 'text',
+  icon: 'warning',
+  confirmButtonColor: '#3085d6',
+  confirmButtonText: 'Confirmar!'
+}).then((result) => {
+  if (result.isConfirmed) {
+
+    Swal.fire(
+      'BEM-VINDO!',
+      '',
+      'success'
+    )
+  }
+})
+}
+  
