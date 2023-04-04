cf16eb38549c46647f7d606787d9ffcaf31ca61d
WolfWillian <willianhidekigg@gmail.com>
2023-04-03 22:08:42 -0300

    Alert mult sub, Botoes

--- projects/tela_bem-Vindo/codigo.js
+++ projects/tela_bem-Vindo/codigo.js
@@ -3,17 +3,16 @@ Nightly.isDark = true;
 settingsMenuHtml =
   '<input type="checkbox" onclick="Nightly.isDark = !Nightly.isDark; setCSSLink()">';
 
-/*var a = 10;
-var b = 20;
-var c = a * b;
-alert("o valor é 1: " + 10);
- alert(c);
-no alerta já soma os valores*/
+
+
+
+
 
 var nome;
 async function alert() {
   const { value: nome } = await Swal.fire({
     icon: "question",
+    iconColor: "yellow",
     input: "text",
     title: "Qual seu nome?",
     inputPlaceholder: "escreva seu nome aqui",
@@ -29,3 +28,71 @@ async function alert() {
     });
   }
 }
+
+async function alertaSub() {
+  const { value: formValues } = await Swal.fire({
+    icon: "question",
+    iconColor: "yellow",
+    title: "Subtração de Valores",
+    html:
+      '<input id="valor1" class="swal2-input">' +
+      "-" +
+      '<input id="valor2" class="swal2-input">',
+    focusConfirm: false,
+    background:
+      "#fff  url(https://s4.static.brasilescola.uol.com.br/be/2023/02/sinal-de-mais-menos-vezes-e-divisao-as-quatro-operacoes-matematicas-basicas-em-blocos-de-madeira-coloridos.jpg)",
+      color: "white",
+    preConfirm: () => {
+      return [
+        document.getElementById("valor1").value -
+          document.getElementById("valor2").value,
+      ];
+    },
+  });
+
+  if (formValues) {
+    Swal.fire(JSON.stringify(formValues), {
+      icon: "success",
+      background:
+        "#fff url(https://s4.static.brasilescola.uol.com.br/be/2023/02/sinal-de-mais-menos-vezes-e-divisao-as-quatro-operacoes-matematicas-basicas-em-blocos-de-madeira-coloridos.jpg)",
+      title:
+        document.getElementById("valor1").value +
+        document.getElementById("valor2").value,
+    });
+  }
+}
+
+async function alertaMult() {
+  const { value: formValues } = await Swal.fire({
+    icon: "question",
+    iconColor: "yellow",
+    title: "Multiplicação de Valores",
+    html:
+      '<input id="valor1" class="swal2-input">' +
+      "*" +
+      '<input id="valor2" class="swal2-input">',
+    focusConfirm: false,
+    background:
+      "#fff url(https://s4.static.brasilescola.uol.com.br/be/2023/02/sinal-de-mais-menos-vezes-e-divisao-as-quatro-operacoes-matematicas-basicas-em-blocos-de-madeira-coloridos.jpg)",
+    color: "white",
+    preConfirm: () => {
+      return [
+        document.getElementById("valor1").value *
+          document.getElementById("valor2").value,
+      ];
+    },
+  });
+
+  if (formValues) {
+    Swal.fire(JSON.stringify(formValues), {
+      icon: "success",
+      iconColor: "green",
+      title:
+        document.getElementById("valor1").value +
+        document.getElementById("valor2").value,
+      background:
+        "#fff url(https://s4.static.brasilescola.uol.com.br/be/2023/02/sinal-de-mais-menos-vezes-e-divisao-as-quatro-operacoes-matematicas-basicas-em-blocos-de-madeira-coloridos.jpg)",
+    });
+  }
+}
+
