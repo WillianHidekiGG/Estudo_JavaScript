acb47926607f47067d3b404468fd6c922f5acf82
WolfWillian <willianhidekigg@gmail.com>
2023-04-04 00:13:04 -0300

    Layout e background

--- projects/tela_confirmar/confirma.js
+++ projects/tela_confirmar/confirma.js
@@ -1,4 +1,7 @@
-var sair = confirm("DEseja sair dessa pagina e ir para o Goggle?"); 
+var sair = confirm("Deseja sair dessa pagina e ir para o Goggle?"); 
+function alert(){
+    sair;
 if(sair == 1){
     window.location = "https://www.google.com/webhp?authuser=3";
+}
 }
