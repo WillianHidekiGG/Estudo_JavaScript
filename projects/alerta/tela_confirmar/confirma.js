57bd93c3a9b4fdec619e8c42398b877f5bfd448f
WolfWillian <willianhidekigg@gmail.com>
2023-04-11 05:57:12 -0300

    Imagem da Logo, Botão arrumado

--- projects/alerta/tela_confirmar/confirma.js
+++ projects/alerta/tela_confirmar/confirma.js
@@ -1,5 +1,5 @@
-var sair = confirm("Deseja sair dessa pagina e ir para o Goggle?"); 
 function alert(){
+    var sair = confirm("Deseja sair dessa pagina e ir para o Goggle?"); 
     sair;
 if(sair == 1){
     window.location = "https://www.google.com/webhp?authuser=3";
