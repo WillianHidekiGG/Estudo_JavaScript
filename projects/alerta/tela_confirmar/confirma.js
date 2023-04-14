b89ee8e977862c2e19f502f3747592b712b02ef5
WolfWillian <willianhidekigg@gmail.com>
2023-04-14 00:15:18 -0300

    Alerta Costomizado

--- projects/alerta/tela_confirmar/confirma.js
+++ projects/alerta/tela_confirmar/confirma.js
@@ -1,7 +1,26 @@
-function alert(){
-    var sair = confirm("Deseja sair dessa pagina e ir para o Goggle?"); 
-    sair;
-if(sair == 1){
-    window.location = "https://www.google.com/webhp?authuser=3";
-}
+async function alert(){
+     
+    const { value: sair } = await Swal.fire({
+      icon: "question",
+      iconColor: "black",
+      background:
+        "#fff url(https://www.trustedreviews.com/wp-content/uploads/sites/54/2018/04/Google-Logo.jpg) center",
+      title: "Você deseja ir para o google?",
+      showCancelButton: true,
+      focusConfirm: false,
+      reverseButtons: true,
+      focusCancel: true,
+      confirmButtonColor: "#00CED1",
+      cancelButtonColor: "#FF6347",
+      cancelButtonText: `Cancelar`,
+      confirmButtonText: `Ir para o GOOGLE`,
+      color:"black",
+      customClass: {
+        popup: "alerta",
+      },
+    }).then((result) => {
+      if (result.value) {
+        window.location.href = "https://www.google.com/webhp?authuser=3";
+      }
+    });
 }
