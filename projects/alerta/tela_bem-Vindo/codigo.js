61f2cf820afd3be54ae10feab9889f9ba4b58aaf
WolfWillian <willianhidekigg@gmail.com>
2023-04-17 22:57:05 -0300

    Animação Alertas, Ajustes no Alerta e Fonte

--- projects/alerta/tela_bem-Vindo/codigo.js
+++ projects/alerta/tela_bem-Vindo/codigo.js
@@ -1,6 +1,9 @@
 var nome;
 async function alert() {
   const { value: nome } = await Swal.fire({
+    showClass: {
+      popup: "animate__animated animate__wobble",
+    },
     background: "#5F9EA0",
     icon: "question",
     iconColor: "yellow",
@@ -8,20 +11,81 @@ async function alert() {
     title: "Qual seu nome?",
     inputPlaceholder: "escreva seu nome aqui",
     showCancelButton: true,
+    color: "black",
+    customClass: {
+      container: "alerta",
+    },
+    hideClass: {
+      popup: "animate__animated animate__zoomOut",
+    },
   });
 
   if (nome) {
     Swal.fire({
+      showClass: {
+        popup: "animate__animated animate__heartBeat",
+      },
       title: "Bem-Vindo " + nome + " !",
       icon: "success",
+      color: "Black",
+      hideClass: {
+        popup: "animate__animated animate__zoomOut",
+      },
+      customClass: {
+        container: "alerta",
+      },
       background:
         "#fff url(https://static8.depositphotos.com/1040728/935/i/450/depositphotos_9352351-stock-photo-party-balloons-flying.jpg)",
     });
+
   }
 }
 
+/*async function alertaSoma() {
+  const { value: formValues } = await Swal.fire({
+    showClass: {
+      popup: "animate__animated animate__lightSpeedInLeft",
+    },
+    icon: "question",
+    iconColor: "yellow",
+    title: "Soma de Valores",
+    html:
+      '<input id="valor1" class="swal2-input">' +
+      "+" +
+      '<input id="valor2" class="swal2-input">',
+    focusConfirm: false,
+    background:
+      "#fff  url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRfe9r_tycLUQjMLiasGmjNPnYiZWXyksezy_h8yvLyheCLgHQheHBlxAPeSeE1Er01_5c&usqp=CAU) center ",
+    color: "Black",
+    hideClass: {
+      popup: "animate__animated animate__lightSpeedOutRight",
+    },
+    preConfirm: () => {
+      Swal.fire({
+        showClass: {
+          popup: "animate__animated animate__lightSpeedInLeft",
+        },
+        icon: "success",
+        title: "O resultado é:",
+        text:
+          document.getElementById("valor1").value +
+          document.getElementById("valor2").value,
+        background:
+          "#fff  url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRfe9r_tycLUQjMLiasGmjNPnYiZWXyksezy_h8yvLyheCLgHQheHBlxAPeSeE1Er01_5c&usqp=CAU) center ",
+        color: "Black",
+        hideClass: {
+          popup: "animate__animated animate__lightSpeedOutRight",
+        },
+      });
+    },
+  });
+}*/
+
 async function alertaSub() {
   const { value: formValues } = await Swal.fire({
+    showClass: {
+      popup: "animate__animated animate__lightSpeedInLeft",
+    },
     icon: "question",
     iconColor: "yellow",
     title: "Subtração de Valores",
@@ -32,29 +96,62 @@ async function alertaSub() {
     focusConfirm: false,
     background:
       "#fff  url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRfe9r_tycLUQjMLiasGmjNPnYiZWXyksezy_h8yvLyheCLgHQheHBlxAPeSeE1Er01_5c&usqp=CAU) center ",
-    color: "#000080",
+    color: "Black",
+    customClass: {
+      container: "alerta",
+    },
+    hideClass: {
+      popup: "animate__animated animate__lightSpeedOutRight",
+    },
     preConfirm: () => {
-      return [
-        document.getElementById("valor1").value -
+      Swal.fire({
+        showClass: {
+          popup: "animate__animated animate__lightSpeedInLeft",
+        },
+        icon: "success",
+        title: "O resultado é:",
+        text:
+          document.getElementById("valor1").value -
           document.getElementById("valor2").value,
-      ];
+        background:
+          "#fff  url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRfe9r_tycLUQjMLiasGmjNPnYiZWXyksezy_h8yvLyheCLgHQheHBlxAPeSeE1Er01_5c&usqp=CAU) center ",
+        color: "Black",
+        customClass: {
+          container: "alerta",
+        },
+        hideClass: {
+          popup: "animate__animated animate__lightSpeedOutRight",
+        },
+      });
     },
   });
 
   if (formValues) {
     Swal.fire(JSON.stringify(formValues), {
+      showClass: {
+        popup: "animate__animated animate__lightSpeedInLeft",
+      },
       icon: "success",
       background:
         "#fff url(https://s4.static.brasilescola.uol.com.br/be/2023/02/sinal-de-mais-menos-vezes-e-divisao-as-quatro-operacoes-matematicas-basicas-em-blocos-de-madeira-coloridos.jpg) center no-repeat",
       title:
         document.getElementById("valor1").value +
         document.getElementById("valor2").value,
+      customClass: {
+        container: "alerta",
+      },
+      hideClass: {
+        popup: "animate__animated animate__lightSpeedOutRight",
+      },
     });
   }
 }
 
 async function alertaMult() {
   const { value: formValues } = await Swal.fire({
+    showClass: {
+      popup: "animate__animated animate__lightSpeedInLeft",
+    },
     icon: "question",
     iconColor: "yellow",
     title: "Multiplicação de Valores",
@@ -65,24 +162,79 @@ async function alertaMult() {
     focusConfirm: false,
     background:
       "#fff  url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRfe9r_tycLUQjMLiasGmjNPnYiZWXyksezy_h8yvLyheCLgHQheHBlxAPeSeE1Er01_5c&usqp=CAU) center ",
-    color: "#000080",
+    color: "Black",
+    customClass: {
+      container: "alerta",
+    },
+    hideClass: {
+      popup: "animate__animated animate__lightSpeedOutRight",
+    },
     preConfirm: () => {
-      return [
-        document.getElementById("valor1").value *
+      Swal.fire({
+        showClass: {
+          popup: "animate__animated animate__lightSpeedInLeft",
+        },
+        icon: "success",
+        title: "O resultado é:",
+        text:
+          document.getElementById("valor1").value *
           document.getElementById("valor2").value,
-      ];
+        background:
+          "#fff  url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRfe9r_tycLUQjMLiasGmjNPnYiZWXyksezy_h8yvLyheCLgHQheHBlxAPeSeE1Er01_5c&usqp=CAU) center ",
+        color: "Black",
+        customClass: {
+          container: "alerta",
+        },
+        hideClass: {
+          popup: "animate__animated animate__lightSpeedOutRight",
+        },
+      });
     },
   });
+}
 
-  if (formValues) {
-    Swal.fire(JSON.stringify(formValues), {
-      icon: "success",
-      iconColor: "green",
-      title:
-        document.getElementById("valor1").value +
-        document.getElementById("valor2").value,
-      background:
-        "#fff url(https://s4.static.brasilescola.uol.com.br/be/2023/02/sinal-de-mais-menos-vezes-e-divisao-as-quatro-operacoes-matematicas-basicas-em-blocos-de-madeira-coloridos.jpg) center no-repeat",
+async function alertaDiv() {
+  const { value: formValues } = await Swal.fire({
+    showClass: {
+      popup: "animate__animated animate__lightSpeedInLeft",
+    },
+    icon: "question",
+    iconColor: "yellow",
+    title: "Divisão de Valores",
+    html:
+      '<input id="valor1" class="swal2-input">' +
+      "/" +
+      '<input id="valor2" class="swal2-input">',
+    focusConfirm: false,
+    background:
+      "#fff  url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRfe9r_tycLUQjMLiasGmjNPnYiZWXyksezy_h8yvLyheCLgHQheHBlxAPeSeE1Er01_5c&usqp=CAU) center ",
+    color: "Black",
+    customClass: {
+      container: "alerta",
+    },
+    hideClass: {
+      popup: "animate__animated animate__lightSpeedOutRight",
+    },
+    preConfirm: () => {
+      Swal.fire({
+        showClass: {
+          popup: "animate__animated animate__lightSpeedInLeft",
+        },
+        icon: "success",
+        title: "O resultado é:",
+        text:
+          document.getElementById("valor1").value /
+          document.getElementById("valor2").value,
+        background:
+          "#fff  url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRfe9r_tycLUQjMLiasGmjNPnYiZWXyksezy_h8yvLyheCLgHQheHBlxAPeSeE1Er01_5c&usqp=CAU) center ",
+        color: "Black",
+        customClass: {
+          container: "alerta",
+        },
+        hideClass: {
+          popup: "animate__animated animate__lightSpeedOutRight",
+        },
       });
-  }
+    },
+  });
 }
