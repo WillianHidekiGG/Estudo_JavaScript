ca5cbd48a8a73a1c523e28d55af1365e45c923cb
Willian Hideki <willianhidekigg@gmail.com>
2023-09-29 18:29:42 -0300

    Alteração não informa dados

--- projects/eventos/calcularIMC/imc.js
+++ projects/eventos/calcularIMC/imc.js
@@ -1,26 +1,54 @@
-function calcularIMC() {
-        document.getElementById("peso").style.borderColor = "white";
-        document.getElementById("peso").style.backgroundColor = "white";
-            document.getElementById("altura").style.borderColor = "white";
-            document.getElementById("altura").style.backgroundColor = "white";
-  if (document.getElementById("peso").value == "") {
-    alert("Preencha peso");
+async function calcularIMC() {
+  var peso = parseFloat(document.getElementById("peso").value);
+  var altura = parseFloat(document.getElementById("altura").value);
+  var imc = altura
+  document.getElementById("altura").style.borderColor = "#ffffff";
+  document.getElementById("altura").style.backgroundColor = "#ffffff";
+  document.getElementById("peso").style.borderColor = "#ffffff";
+  document.getElementById("peso").style.backgroundColor = "#ffffff";
+
+
+Swal.fire({
+  didOpen: () =>{
+    if(peso == ""){
     document.getElementById("peso").style.borderColor = "red";
     document.getElementById("peso").style.backgroundColor = "#ffa5e5";
     document.getElementById("peso").focus;
-    return false;
-  }
-  if (document.getElementById("altura").value == "") {
-    alert("Preencha altura");
+    Swal.fire({
+      icon:"warning",
+      title:"Falta infomação",
+      text:"Coloque seu peso",
+    });
+  }else if(altura == ""){
     document.getElementById("altura").style.borderColor = "red";
     document.getElementById("altura").style.backgroundColor = "#ffa5e5";
     document.getElementById("altura").focus;
-    return false;
+    Swal.fire({
+      icon:"warning",
+      title:"Falta infomação",
+      text:"Coloque seu altura",
+    });
+  };
+
+  
+   if(imc == 18){
+    Swal.fire({
+      icon:"warning",
+      title:"ABAIXO DO PESO",
+      text:"Seu IMC é " + imc + ", consulte um profissional da saude para mais orientação",
+    });
   }
-  var peso = parseFloat(document.getElementById("peso").value);
-  var altura = parseFloat(document.getElementById("altura").value);
-  var resultado = peso/(altura*altura);
-  alert("seu IMC é: "+resultado);
-  return true;
+
+  },
+
+    });
+    
+    
+
+  // var peso = parseFloat(document.getElementById("peso").value);
+  // var altura = parseFloat(document.getElementById("altura").value);
+  // var resultado = peso/(altura*altura);
+  // alert("seu IMC é: "+resultado);
+  // return true;
   
 }
